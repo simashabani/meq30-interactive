6f5bbeb509b841541041711430dd53ce1647c6e2
Sima Shabani <sima.shabani@gmail.com>
2026-02-06 09:07:22 -0500

    first test comment

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -7,7 +7,6 @@ export async function GET(request: Request) {
   const code = url.searchParams.get("code");
 
   const cookieStore = await cookies();
-//testing this
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
