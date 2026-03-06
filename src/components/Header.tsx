24d89156df35ae649b94869f422bc57ed8b4486a
Sima Shabani <sima.shabani@gmail.com>
2026-03-06 11:06:04 -0500

    email top margin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -621,7 +621,6 @@ export default function Header({ locale }: Props) {
   }
 
   .user-menu-email-logged {
-    margin-top: 10px !important;
     margin-bottom: 2px;
     font-size: 16px;
     font-weight: 700;
